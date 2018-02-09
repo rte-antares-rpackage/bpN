369703bf65cd13b2a73c619e0f1f90a6f6a8b450
B.Thieurmel <bthieurmel@gmail.com>
2018-02-09 16:59:10 +0100

    set thousand separator to ' '

--- inst/application_bp/src/server/02_hypotheses.R
+++ inst/application_bp/src/server/02_hypotheses.R
@@ -5,13 +5,15 @@ output$hyp_prod <- renderAmCharts({
   isolate({
     res <- getProductionHypothesis(data = hyp_prod, nodes = input$area_hyp_prod, sce_prod = sce_prod, scenario = input$hyp_scenario)
     
-    amBarplot(x = "date", y = colnames(res)[-1], data = res, 
+    gr  <- amBarplot(x = "date", y = colnames(res)[-1], data = res, 
               stack_type = "regular", legend = TRUE,
               groups_color = unname(cl_hyp_prod[colnames(res)[-1]]), 
               main = paste0("Evolution du parc installé (scénario ", input$hyp_scenario, ")"),
               zoom = TRUE, export = TRUE, show_values = FALSE,
               ylab = "MW",
               labelRotation = 45, legendPosition = "bottom", height = "800")
+    gr@otherProperties$thousandsSeparator <- " "
+    gr
   })
 })
 
@@ -19,13 +21,15 @@ output$hyp_conso <- renderAmCharts({
   
     res <- getConsoHypothesis(data = hyp_conso, sce_prod = sce_prod, scenario = input$hyp_scenario)
     
-    amBarplot(x = "date", y = colnames(res)[-1], data = res, 
+    gr  <- amBarplot(x = "date", y = colnames(res)[-1], data = res, 
               stack_type = "regular", legend = TRUE,
               groups_color = unname(cl_hyp_prod[1:(ncol(res) - 1)]), 
               main = paste0("Hypothèses de consommation (scénario ", input$hyp_scenario, ")"),
               zoom = TRUE, export = TRUE, show_values = FALSE,
               ylab = "TWh",
               labelRotation = 45, legendPosition = "bottom", height = "800")
+    gr@otherProperties$thousandsSeparator <- " "
+    gr
 })
 
 res_inter <- reactive({
@@ -36,24 +40,28 @@ output$hyp_inter_import <- renderAmCharts({
   
   res <- res_inter()$import
   
-  amBarplot(x = "date", y = colnames(res)[-1], data = res, 
+  gr  <- amBarplot(x = "date", y = colnames(res)[-1], data = res, 
             stack_type = "regular", legend = TRUE,
             groups_color = unname(cl_hyp_interco[1:(ncol(res) - 1)]), 
             main = paste0("Evolution des capacités d'import (scénario ", input$hyp_scenario, ")"),
             zoom = TRUE, export = TRUE, show_values = FALSE,
             ylab = "MW",
             labelRotation = 45, legendPosition = "bottom", height = "800")
+  gr@otherProperties$thousandsSeparator <- " "
+  gr
 })
 
 output$hyp_inter_export <- renderAmCharts({
   
   res <- res_inter()$export
   
-  amBarplot(x = "date", y = colnames(res)[-1], data = res, 
+  gr  <- amBarplot(x = "date", y = colnames(res)[-1], data = res, 
             stack_type = "regular", legend = TRUE,
             groups_color = unname(cl_hyp_interco[1:(ncol(res) - 1)]), 
             main = paste0("Evolution des capacités d'export (scénario ", input$hyp_scenario, ")"),
             zoom = TRUE, export = TRUE, show_values = FALSE,
             ylab = "MW",
             labelRotation = 45, legendPosition = "bottom", height = "800")
+  gr@otherProperties$thousandsSeparator <- " "
+  gr
 })
