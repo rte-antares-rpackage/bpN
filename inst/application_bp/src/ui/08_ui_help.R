1bccf7218dbb9cebff57dd68ab47e46d97f3b0b7
B.Thieurmel <bthieurmel@gmail.com>
2018-02-16 15:56:43 +0100

    rm br

--- inst/application_bp/src/ui/08_ui_help.R
+++ inst/application_bp/src/ui/08_ui_help.R
@@ -1,7 +1,6 @@
 tabPanel("A propos", 
          fluidRow(
            column(width = 12,
-                  br(),
                   includeMarkdown("src/aide/a_propos.md")
            )
          )
