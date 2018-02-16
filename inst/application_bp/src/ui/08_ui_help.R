7e326fd9b3e432444be0d316ee4489b16e6f055c
B.Thieurmel <bthieurmel@gmail.com>
2018-02-16 15:54:51 +0100

    #56  : add info about antares

--- inst/application_bp/src/ui/08_ui_help.R
+++ inst/application_bp/src/ui/08_ui_help.R
@@ -1,21 +1,8 @@
 tabPanel("A propos", 
          fluidRow(
            column(width = 12,
-                  "TO DO"
-                  # tabsetPanel(
-                  #   tabPanel("Production", 
-                  #            fluidRow(
-                  #              column(12, br(), includeMarkdown("src/aide/production.md"))
-                  #            )
-                  #   ),
-                  #   tabPanel("Echanges", 
-                  #            fluidRow(
-                  #              column(12, includeMarkdown("src/aide/echanges.md"))
-                  #            )
-                  #   )
-                  # )
-                  
+                  br(),
+                  includeMarkdown("src/aide/a_propos.md")
            )
-           
          )
 )
