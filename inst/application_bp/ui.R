18baf7e205967b4c5702af8e14ffd1d6f3a44c5a
Benoit Thieurmel <bthieurmel@gmail.com>
2018-03-29 10:49:11 +0200

    #101 : css

--- inst/application_bp/ui.R
+++ inst/application_bp/ui.R
@@ -1,16 +1,18 @@
 # Define UI for bpNumerique2018 app
-navbarPage(title = "Bilan prévisionnel 2017", id = "nav-id", theme = "css/custom.css", collapsible = TRUE, position = "fixed-top",
+navbarPage(title = HTML('<p style="margin-top: 0.05cm;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Bilan prévisionnel 2017</p>'), id = "nav-id", theme = "css/custom.css", collapsible = TRUE, position = "fixed-top",
            header = div(
-             br(), br(), br(), br(), a(href = "http://www.rte-france.com/fr/article/bilan-previsionnel",
-                                       target = "_blank", img(src = "img/RTE_logo.svg.png", class = "ribbon")),
+             br(), br(), br(), br(), br(), a(href = "http://www.rte-france.com/fr/article/bilan-previsionnel",
+                                       target = "_blank", img(src = "img/Logo_RTE.png", class = "ribbon")),
              singleton(tags$script(src = 'events.js')),
              singleton(tags$script(src = 'is.min.js')),
              tags$script(type="text/javascript", 'if(is.ie()){ alert("Ce site n\'est pas optimisé pour Internet Explorer");};'),
              div(id = "import_busy", tags$img(src= "spinner.gif", height = 100,
                                               style = "position: fixed;top: 50%;z-index:10;left: 48%;")),
              
-             div(class = "rte_footer", HTML("Plus d'informations sur la page <a href='http://www.rte-france.com/fr/article/bilan-previsionnel' target='_blank' style = 'color:black'>www.rte-france.com/fr/article/bilan-previsionnel</a>   
-                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Les données présentées sont téléchargeables sur la plateforme <a href='https://opendata.reseaux-energies.fr/pages/accueil/' target='_blank' style = 'color:black'>Open Data Réseaux Energies</a>"))
+             # div(class = "rte_footer", HTML("Plus d'informations sur la page <a href='http://www.rte-france.com/fr/article/bilan-previsionnel' target='_blank'>www.rte-france.com/fr/article/bilan-previsionnel</a>   
+             #                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Les données présentées sont téléchargeables sur la plateforme <a href='https://opendata.reseaux-energies.fr/pages/accueil/' target='_blank' style = 'color:black'>Open Data Réseaux Energies</a>"))
+             
+             div(class = "rte_footer", HTML("Plus d'informations sur la page <a href='http://www.rte-france.com/fr/article/bilan-previsionnel' target='_blank'>www.rte-france.com/fr/article/bilan-previsionnel</a>"))
              
              # ), windowTitle = div(img(src="img/RTE_logo.svg.png"), "BP 2017 RTE - Accueil"),
            ),windowTitle = "BP 2017 RTE - Accueil",
