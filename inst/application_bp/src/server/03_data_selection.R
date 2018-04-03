10714aaf3406fec017fd79d5585d9960b38a642f
Benoit Thieurmel <bthieurmel@gmail.com>
2018-04-03 10:32:04 +0200

    #104

--- inst/application_bp/src/server/03_data_selection.R
+++ inst/application_bp/src/server/03_data_selection.R
@@ -6,8 +6,9 @@ output$list_study <- renderUI({
   if(length(list_data) > 0){
     choices = names(list_data)
     names(choices) <- gsub(".h5$", "", choices)
+    choices <- choices[c(which(!grepl("^Ohm", choices)), which(grepl("^Ohm", choices)))]
     isolate({
-      selectizeInput("sel_study", label = NULL, choices = choices, selected = choices[1], multiple = TRUE, 
+      selectizeInput("sel_study", label = NULL, choices = choices, selected = NULL, multiple = TRUE, 
                      width = "100%", options = list(maxItems = 4))
     })
   }
